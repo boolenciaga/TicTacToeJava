f40379a21662ceb66c960c42b39ab831ff1144fe
ChanPhillip <chnphillip@gmail.com>
2020-04-03 23:23:17 -0700

    Pause JavaFX Window

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,9 +15,9 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+//
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
 }
