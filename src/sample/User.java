4796dc425abb41db783a1e2d3ff14057c848822e
ChanPhillip <chnphillip@gmail.com>
2020-04-03 23:34:39 -0700

    Added Set methods

--- src/sample/User.java
+++ src/sample/User.java
@@ -7,4 +7,23 @@ public class User {
     private String firstName;
     private String lastName;
 
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
 }
