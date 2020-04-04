b706f099ffc1dc5bc40bb9543a80f09694b9a8f2
ChanPhillip <chnphillip@gmail.com>
2020-04-03 23:25:25 -0700

    Added conditional instantiation

--- src/sample/DatabaseManager.java
+++ src/sample/DatabaseManager.java
@@ -1,4 +1,30 @@
 package sample;
 
 public class DatabaseManager {
+    private static DatabaseManager instance = null;
+
+    private DatabaseManager() {
+
+    }
+
+    public static DatabaseManager getInstance()
+    {
+        if (instance == null)
+        {
+            synchronized (DatabaseManager.class)
+            {
+                if (instance == null)
+                {
+                    instance = new DatabaseManager();
+                }
+            }
+        }
+
+        return instance;
+
+    }
+
+
+
+
 }
