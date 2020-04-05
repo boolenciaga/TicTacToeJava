dd354d2800b70ed1431095bbd35a50377fcfb4bd
Kenneth Casimiro <36016735+boolenciaga@users.noreply.github.com>
2020-04-04 23:47:43 -0700

    Add javaFX skeleton code to addUserController

--- GUI/src/sample/addUserController.java
+++ GUI/src/sample/addUserController.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class addUserController {
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private Button createUser;
+
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    void createrUserClicked(ActionEvent event) {
+
+    }
+
 }
