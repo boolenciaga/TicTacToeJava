49f1371abbe63ededdec824e09fc8152504dd255
ChanPhillip <chnphillip@gmail.com>
2020-04-06 14:03:15 -0700

    Added Error Label to Create User Page

--- src/sample/addUserController.java
+++ src/sample/addUserController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import java.sql.SQLException;
+
 public class addUserController {
 
     @FXML
@@ -25,6 +27,21 @@ public class addUserController {
     @FXML
     void createrUserClicked(ActionEvent event) {
 
+        if(!username.getText().isBlank() && !password.getText().isBlank() && !firstName.getText().isBlank() &&
+           !lastName.getText().isBlank())
+        {
+            User newUser = new User(username.getText(), password.getText(), firstName.getText(), lastName.getText());
+            DatabaseManager.getInstance();
+            try {
+                DatabaseManager.addUser(newUser);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+
+        }
     }
 
 }
