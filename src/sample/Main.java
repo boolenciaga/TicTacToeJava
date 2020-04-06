ccc041b0757e78fa5969cd4a00293443701028f5
ChanPhillip <chnphillip@gmail.com>
2020-04-06 13:43:41 -0700

    Make Creation Page Open when app starts

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,10 +14,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("addUserWindow.fxml"));
+        primaryStage.setTitle("Add User Record Window");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
 
