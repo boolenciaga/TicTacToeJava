6c206ec45c3ae14b98b2db0348bb5a0a2d760c29
ChanPhillip <chnphillip@gmail.com>
2020-04-06 00:05:01 -0700

    Added Fields to Moves Class

--- src/sample/Moves.java
+++ src/sample/Moves.java
@@ -1,4 +1,13 @@
 package sample;
 
+import java.util.Date;
+
 public class Moves {
+    private int moveId;
+    private int gameId;
+    private int playerId;
+    private int Xcoord;
+    private int Ycoord;
+    private Date time;
+
 }
