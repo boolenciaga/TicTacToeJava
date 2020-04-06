944c8c14ca22b7c9485fd8fa858a80e12966a87e
ChanPhillip <chnphillip@gmail.com>
2020-04-06 00:02:38 -0700

    Added fields for Game Class

--- src/sample/Game.java
+++ src/sample/Game.java
@@ -1,4 +1,16 @@
 package sample;
 
+import java.util.Date;
+
 public class Game {
+    private int  gameId;
+    private Date startTime;
+    private Date endTime;
+    private int  p1Id;
+    private int p2Id;
+    private int starterId;
+    private int winnerId;
+
+    
+
 }
